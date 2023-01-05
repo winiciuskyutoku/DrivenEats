d8cd861677f3edc51fb953a7b07fe380d322c319
“winiciuskyutoku“ <winicius123098@gmail.com>
2023-01-05 14:05:22 -0300

    Mensagem do Pedido Final Concluído

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let comida, preco1Numero, bebida, preco2Numero, sobremesa, preco3Numero, soma, somaCorrigida
+
 function selecionado(pratoSelecionado) {
     const primeiroSelecionado1 = document.querySelector(".bordaVerde1");
 
@@ -8,6 +10,12 @@ function selecionado(pratoSelecionado) {
    const teste1 = document.querySelector(pratoSelecionado);
    console.log(teste1);
    teste1.classList.toggle('bordaVerde1');
+
+    comida = teste1.querySelector("h2").innerHTML;
+   let preco1 = teste1.querySelector("strong").innerHTML;
+    preco1Numero = Number(preco1.replace(",","."));
+   console.log(comida);
+   console.log(preco1Numero)
 
     verificar();
 
@@ -23,6 +31,12 @@ function selecionado2(bebidaSelecionado) {
    const teste2 = document.querySelector(bebidaSelecionado);
    console.log(teste2);
    teste2.classList.toggle('bordaVerde2');
+
+   bebida = teste2.querySelector("h2").innerHTML;
+    let preco2 = teste2.querySelector("strong").innerHTML;
+    preco2Numero = Number(preco2.replace(",","."));
+   console.log(bebida);
+   console.log(preco2Numero);
 
    verificar(); 
 
@@ -40,6 +54,13 @@ function selecionado3(sobremesaSelecionado) {
    console.log(teste3);
    teste3.classList.toggle('bordaVerde3');
 
+   sobremesa = teste3.querySelector("h2").innerHTML;
+   preco3 = teste3.querySelector("strong").innerHTML;
+   preco3Numero = Number(preco3.replace(",","."));
+   
+   console.log(sobremesa);
+   console.log(preco3Numero);
+
    verificar();
 
 
@@ -49,6 +70,7 @@ function verificar() {
     const verificar1 = document.querySelector(".bordaVerde1");
     const verificar2 = document.querySelector(".bordaVerde2");
     const verificar3 = document.querySelector(".bordaVerde3");
+
 
     if (verificar1 !== null && verificar2 !== null && verificar3 !== null) {
         const mudarBotao = document.querySelector(".botaoPedido");
@@ -61,6 +83,23 @@ function verificar() {
         finalizarBotao1.classList.remove('finalizarPedido1')
         finalizarBotao1.classList.add("aparecendo");
         
+        calculadorPedido();
+
+        trocarURL();
+
     }   
 }
+
+function calculadorPedido() {
+    soma = preco1Numero + preco2Numero + preco3Numero;
+    somaCorrigida = soma.toFixed(2);
+    console.log(somaCorrigida);
+}
+
+function trocarURL() {
+    let novoURL = `https://wa.me/5544988598008?text=Olá,%20gostaria%20de%20fazer%20o%20pedido:%0A-%20Prato:%20 ${comida} %0A-%20Bebida:%20 ${bebida} %0A-%20Sobremesa:%20 ${sobremesa} %0ATotal:%20R$%20 ${somaCorrigida}`;
+    document.getElementById("finalizarPedido1").href = novoURL;
+}
+
+
 
